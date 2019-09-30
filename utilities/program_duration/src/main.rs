a445942542ff88856a522c3e3699e5de37a3795a
Lars Tobias Skjong-Børsting <larstobi@relatime.no>
2019-09-30 21:17:34 +0200

    Updated it

--- utilities/program_duration/src/main.rs
+++ utilities/program_duration/src/main.rs
@@ -1,9 +1,25 @@
-extern crate yaml_rust;
-use yaml_rust::{YamlLoader, YamlEmitter};
+extern crate serde;
+extern crate serde_yaml;
+extern crate serde_derive;
+use serde::{Deserialize, Serialize};
 use std::{fs::File, io::Read};
-// extern crate serde_yaml;
+
 // extern crate chrono;
 // use chrono::{Duration, Utc};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct DurationEntry {
+    title: String,
+    date: String,
+    duration: u8,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct DurationProgram {
+    program_start_time: String,
+    program: Vec<DurationEntry>,
+}
+
 
 fn main()  {
     // let now = Utc::now();
@@ -11,38 +27,18 @@ fn main()  {
     // let thirty_minutes_from_now = now + Duration::minutes(30);
     // println!("{}", thirty_minutes_from_now);
 
-    // let f = std::fs::File::open("something.yaml")?;
-    // let d: String = serde_yaml::from_reader(f)?;
-    // println!("Read YAML string: {}", d);
-    // Ok(())
-
     let mut f = File::open("something.yaml").expect("Unable to open file");
     let mut s = String::new();
-    f.read_to_string(&mut s).expect("Unable to read file"); 
+    f.read_to_string(&mut s).expect("Unable to read file");
 
-    let docs = YamlLoader::load_from_str(&s).unwrap();
+    let _deserialized_program: Result<DurationProgram, _> = serde_yaml::from_str(&s);
+    println!("{:?}", _deserialized_program);
 
-    // Multi document support, doc is a yaml::Yaml
-    let doc = &docs[0];
+    // // // Index access for map & array
+    // // assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
+    // // assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
 
-    // Debug support
-    println!("{:?}", doc);
-
-    // // Index access for map & array
-    // assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
-    // assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
-
-    // // Chained key/array access is checked and won't panic,
-    // // return BadValue if they are not exist.
-    // assert!(doc["INVALID_KEY"][100].is_badvalue());
-
-    // Dump the YAML object
-    let mut out_str = String::new();
-    {
-        let mut emitter = YamlEmitter::new(&mut out_str);
-        emitter.dump(doc).unwrap(); // dump the YAML object to a String
-    }
-    println!("{}", out_str);
+    // // // Chained key/array access is checked and won't panic,
+    // // // return BadValue if they are not exist.
+    // // assert!(doc["INVALID_KEY"][100].is_badvalue());
 }
-
-
